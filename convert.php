b01c0043706da9b8aaa36cbe9c2ae67d1a3846a3
Victor Klos <victor@victorklos.nl>
2015-03-05 18:41:42 +0100

    normalize path before writing
    See https://github.com/philipashlock/mediawiki-to-markdown/issues/2
    for an explanation of what can go wrong

--- convert.php
+++ convert.php
@@ -111,8 +111,7 @@ while(list( , $node) = each($result)) {
     }
 
     // create file
-
-    $file = fopen($directory . $filename . '.md', 'w');
+    $file = fopen(normalizePath($directory . $filename . '.md'), 'w');
     fwrite($file, $text);
     fclose($file);
 
@@ -170,6 +169,37 @@ function new_link($matches){
 }
 
 
+// Borrowed from http://php.net/manual/en/function.realpath.php
+function normalizePath($path)
+{
+    $parts = array();                         // Array to build a new path from the good parts
+    $path = str_replace('\\', '/', $path);    // Replace backslashes with forwardslashes
+    $path = preg_replace('/\/+/', '/', $path);// Combine multiple slashes into a single slash
+    $segments = explode('/', $path);          // Collect path segments
+    $test = '';                               // Initialize testing variable
+    foreach($segments as $segment)
+    {
+        if($segment != '.')
+        {
+            $test = array_pop($parts);
+            if(is_null($test))
+                $parts[] = $segment;
+            else if($segment == '..')
+            {
+                if($test == '..')
+                    $parts[] = $test;
+                if($test == '..' || $test == '')
+                    $parts[] = $segment;
+            }
+            else
+            {
+                $parts[] = $test;
+                $parts[] = $segment;
+            }
+        }
+    }
+    return implode('/', $parts);
+}
 
 
 ?>
