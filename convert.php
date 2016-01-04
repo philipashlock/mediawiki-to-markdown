502aadd6fd94f596c6a7c9197e292c2d7a288f14
Tim White <timw@suqld.org.au>
2016-01-04 15:12:11 +1000

    Fix for multiple level of directories

--- convert.php
+++ convert.php
@@ -61,7 +61,7 @@ while(list( , $node) = each($result)) {
     $title = $title[0];
     $url = str_replace(' ', '_', $title);
 
-    if($slash = strpos($url, '/')){
+    if($slash = strrpos($url, '/')){
         $title = str_replace('/', ' ', $title);
         $directory = substr($url, 0, $slash);
         $filename = substr($url, $slash+1);
@@ -104,7 +104,7 @@ while(list( , $node) = each($result)) {
     // create directory if necessary
     if(!empty($directory)) {
         if(!file_exists($directory)) {
-            mkdir($directory);
+            mkdir($directory, 0777, true);
         }
 
         $directory = $directory . '/';
